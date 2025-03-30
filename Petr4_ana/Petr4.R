d01e684a8ca3e5f05e8680bae23fcf64fd4cb2c6
Gustavo Vitor da Silva <Gustavoboss066@gmail.com>
2025-03-30 18:50:36 -0300

    vida dificil

--- Petr4_ana/Petr4.R
+++ Petr4_ana/Petr4.R
@@ -19,122 +19,136 @@ library(parsnip)
 library(rsample)      
 library(cowplot)
 
-os = Sys.info()["sysname"]
+# Leitura condicional do arquivo
+os <- Sys.info()["sysname"]
+if(os == "Windows") {
+  dt.intra <- read.csv("D:/Code/R_studio/Petr4_ana/dt_1min_PETR4_2021_metatrader.csv", 
+                       header = TRUE, stringsAsFactors = FALSE, 
+                       sep = ";", dec = ",")
+} else if(os == "Linux") {
+  dt.intra <- read.csv("~/Documentos/Coding/Statistics_in_R/Petr4_ana/dt_1min_PETR4_2021_metatrader.csv", 
+                       header = TRUE, stringsAsFactors = FALSE, 
+                       sep = ";", dec = ",")
+}
 
-if(os == "Windows")
-  data <- data <- read.csv("D:/Code/R_studio/Petr4_ana/dt_1min_PETR4_2021_metatrader.csv", 
-                           header = TRUE, 
-                           stringsAsFactors = FALSE, 
-                           sep = ";",dec = ",")
-
-if (os =="Linux")
-  data <- data <- read.csv("~/Documentos/Coding/Statistics_in_R/Petr4_ana/dt_1min_PETR4_2021_metatrader.csv", 
-                         header = TRUE, 
-                         stringsAsFactors = FALSE, 
-                         sep = ";",dec = ",")
-
-
-dt1 <- as_tibble(data) %>% 
+dt1 <- as_tibble(dt.intra) %>% 
   mutate(Period = ymd_hms(X)) %>%
-  select(-X) 
-#grafico do preço de fechamento
-
-ggplot(dt1, aes(x= Period, y = Close.1min), )+
-  geom_line()+
-  labs(title = "Valor de fechamento Petr4 2021 primeiro semestre")+
-  xlab("Periodo Jan a Jul 2021")+
-  ylab("Valor de fechamento")
-
-
-#gráfico de retornos de log 
-
-price_xts <- as.xts(dt1$Ret.1min, order.by = dt1$Period)
-plot(price_xts, main="PETR4 Preço retorno 1min 2021", ylab="Preço", col="black")
-par(mfrow=c(1,1))
-
-
-#tentando remover as linhas que aparecem entre os dias 
-
-dt1_minusf15 <- as_tibble(data) %>% 
-  mutate(Period = ymd_hms(X)) %>%
-  select(-X) %>%
-  filter(format(Period, "%H:%M:%S") >= "10:15:00",
-         format(Period, "%H:%M:%S") <= "17:45:00") %>%
+  select(-X) %>% 
+  filter(!(hour(Period) == 10 & minute(Period) < 20)) %>% 
+  filter(hour(Period) < 17) %>% 
+  filter(!(hour(Period) == 16 & minute(Period) > 54)) %>% 
+  filter(!(date(Period) == "2021-02-17")) %>% 
   arrange(Period)
 
-dt1_minusf15 <- dt1_minusf15 %>%
-  mutate(obs = row_number())
+ret.1min <- as.xts(dt1$Ret.1min, order.by = dt1$Period)
 
-n_obs <- nrow(dt1_minusf15)
-label_positions <- c(1, round(n_obs/2), n_obs)
-label_times <- format(dt1_minusf15$Period[label_positions], "%m:%d:%H:%M:%S")
+# Teste ADF e visualizações iniciais
+tseries::adf.test(ret.1min)
+plot.ts(ret.1min)
+boxplot(as.double(ret.1min))
 
-ggplot(dt1_minusf15, aes(x = obs, y = Ret.1min)) +
-  geom_line(color = "black", size = 1) +
-  scale_x_continuous(breaks = label_positions, labels = label_times) +
-  labs(x = "Tempo Mes/Dia/Hora/Minuto", y = "Retorno logaritmico",
-       title = "Serie temporal Petr4 2021 (linha entre os dias removida)")
+# Identificar outliers
+idx_min <- which.min(as.double(ret.1min))
+idx_max <- which.max(as.double(ret.1min))
 
-#dt1_minus15f remove todos os tempos de antes de 10:15 e depois de 17:45 para padronização do tempo
+# Substituir outliers pela observação anterior
+ret.1min[idx_min] <- ret.1min[idx_min - 1]
+ret.1min[idx_max] <- ret.1min[idx_max - 1]
 
-#histograma e qxq plot
+plot.ts(ret.1min)
+boxplot(as.double(ret.1min))
+tseries::adf.test(ret.1min) 
 
-
-hist(coredata(dt1_minusf15$Ret.1min), breaks=50, main="Histograma do log de retornos", xlab="Log de retornos", ylab= "Densidade", probability = TRUE,xlim = c(-0.05,0.05))
-lines(density(coredata(dt1_minusf15$Ret.1min)), col="blue", lwd=2)
-qqnorm(coredata(dt1$Ret.1min), main = "Qxq Plot dos retornos"); qqline(coredata(dt1$Ret.1min), col="red")
-
-#Plot do retorno logaritimo mais função de alto correlação e função parcial de alta correlação 
-
-ggtsdisplay(dt1_minusf15$Ret.1min, main = "Retornos 1min brutos")
-
-
-#plot retorno log acf e pacf ao quadrado
-
-ggtsdisplay(dt1_minusf15$Ret.1min^2, main = "Quadrado dos retornos 1min brutos ")
-
-
-#identificação de outliers 
-
-boxplot(coredata(dt1$Ret.1min), main = "Boxplot retornos Petr4")
-dt1_filter <- dt1_minusf15 %>% 
-  filter(Ret.1min > -0.03) %>%
-  filter(Ret.1min < 0.03)%>%
-  na.omit(dt1_minusf15)
-  
-
-#plot retorno filtrado para outliers muito grandes
-
-ggtsdisplay(dt1_filter$Ret.1min, main = "Retornos 1min brutos")
-ggtsdisplay(dt1_filter$Ret.1min^2, main = "Quadrado dos retornos 1min brutos ")
-boxplot(coredata(dt1_filter$Ret.1min), main = "Boxplot retornos Petr4")
-
-
-#filtrando lags para teste Acf e Pacf
-
-n = 5
-log_diff_18 <- diff(log(dt1_minusf15$Close.1min), lag = n)
-
-teste <- data.frame(Retornos = log_diff_18, Periodo = dt1_minusf15$Period[-c(1:n)])
-
-teste <- teste %>%
-  filter(Retornos > -0.03)%>%
-  filter(Retornos < 0.03)%>%
-  na.omit(teste)
-
-ggtsdisplay(teste$Retornos, main = "Retornos 1min brutos")
-ggtsdisplay(teste$Retornos^2, main = "Quadrado dos retornos 1min brutos ")
-
-
-#media movel não obtive muita informação
-
-ggplot(dt1_filter, aes(x = obs, y = Ret.1min)) +
-  geom_line(color = "black", size = 1, alpha = 0.4) +  # raw returns (with some transparency)
-  geom_smooth(method = "loess", span = 0.0001, se = FALSE, color = "darkgreen", size = 1.2) +
-  scale_x_continuous(breaks = label_positions, labels = label_times) +
-  labs(x = "Tempo Mes/Dia/Hora/Minuto", y = "Retorno logaritmico",
-       title = "Série Temporal PETR4 2021 1-min com Suavização LOESS") +
-  theme_minimal()
+# Gráfico de retornos
+plot(ret.1min, main="PETR4 Preço retorno 1min 2021", ylab="Preço", col="black")
 
 
 
+# Converter para vetor numérico
+ret_vec <- as.numeric(ret.1min[1:5000])
+sv_fit <- svsample(ret_vec, draws = 5000, burnin = 1000)
+summary(sv_fit)
+
+par(mfrow=c(2,2))
+plot(sv_fit, showobs = FALSE)
+
+# Extrair volatilidade estimada (média dos draws)
+
+sv_vol_mean <- exp(as.numeric(as.character(sv_fit$latent)) / 2)
+vol_xts <- xts(sv_vol_mean, order.by = index(ret.1min))
+
+# Gráficos dos retornos e volatilidade estimada
+par(mfrow=c(2,1))
+plot(ret.1min, main="PETR4 Retornos 1min 2021", ylab="Retorno", col="black")
+plot(vol_xts, main="PETR4 Volatilidade Estocástica Estimada 1min 2021", 
+     ylab="Volatilidade", col="blue")
+
+# Calcular volatilidade realizada (janela de 30 minutos)
+realized_vol <- rollapply(ret.1min^2, width=30, FUN=function(x) sqrt(252*390*mean(x)), 
+                          by.column=TRUE, align="right")
+realized_vol <- na.omit(realized_vol)
+
+par(mfrow=c(1,1))
+plot(realized_vol, main="Comparação de Volatilidades", 
+     ylab="Volatilidade", col="red")
+lines(vol_xts[index(realized_vol)], col="blue")
+legend("topright", legend=c("Volatilidade Realizada", "Volatilidade Estocástica"), 
+       col=c("red", "blue"), lty=1)
+
+# Análise de clustering de volatilidade
+par(mfrow=c(2,1))
+acf(abs(ret_vec), main="ACF dos Retornos Absolutos")
+pacf(abs(ret_vec), main="PACF dos Retornos Absolutos")
+
+cat("\nTeste ARCH LM para efeitos ARCH:\n")
+ArchTest(ret_vec, lags=10)
+
+# Plotar distribuição posterior dos parâmetros
+par(mfrow=c(2,2))
+plot(sv_fit, param = "mu")
+plot(sv_fit, param = "phi")
+plot(sv_fit, param = "sigma")
+plot(sv_fit, param = "nu")
+
+# Construir dataframe com componentes de volatilidade
+volatility_components <- data.frame(
+  date = index(ret.1min),
+  returns = as.numeric(ret.1min),
+  sv_vol = sv_vol_mean,
+  stringsAsFactors = FALSE
+)
+
+# Identificar períodos de alta volatilidade (top 5%)
+high_vol_periods <- volatility_components %>%
+  as_tibble() %>%
+  mutate(date = as.POSIXct(date)) %>%
+  filter(sv_vol > quantile(sv_vol, 0.95)) %>%
+  arrange(date)
+
+cat("\nPeríodos de volatilidade mais elevada (top 5%):\n")
+print(head(high_vol_periods, 10))
+
+# Resumo estatístico da volatilidade por hora do dia
+vol_hour_summary <- volatility_components %>%
+  as_tibble() %>%
+  mutate(date = as.POSIXct(date),
+         hour = hour(date)) %>%
+  group_by(hour) %>%
+  summarise(mean_vol = mean(sv_vol),
+            median_vol = median(sv_vol),
+            max_vol = max(sv_vol)) %>%
+  arrange(desc(mean_vol))
+
+cat("\nVolatilidade média por hora do dia:\n")
+print(vol_hour_summary)
+
+cat("\nEstatísticas descritivas da volatilidade estimada:\n")
+summary(volatility_components$sv_vol)
+
+par(mfrow=c(1,1))
+hist(volatility_components$sv_vol, breaks=50, 
+     main="Distribuição da Volatilidade Estocástica", 
+     xlab="Volatilidade", col="lightblue")
+
+# Salvar resultados para uso futuro
+save(sv_fit, vol_xts, volatility_components, file = "PETR4_stochastic_volatility_results.RData")
